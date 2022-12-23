80b110441bab06890ff935b9d00e3b46886a896f
datamoshing <imkiloss@gmail.com>
2022-12-23 16:37:41 -0700

    Created headers for inbox, today, and week

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import _ from "lodash";
+// import _ from "lodash";
 import "./style.css";
+import createTask from "./task";
+import { inboxBtn, todayBtn, weekBtn, projectMain, inboxMain, todayMain, weekMain } from "./dom";
 
-function component() {
-    const element = document.createElement("div");
+// function component() {
+//   const element = document.createElement("div");
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(["Hello", "webpack"], " ");
-    element.classList.add("hello");
+//   // Lodash, currently included via a script, is required for this line to work
+//   element.innerHTML = _.join(["Hello", "webpack"], " ");
+//   element.classList.add("hello");
 
-    return element;
+//   return element;
+// }
 
-}
+inboxMain()
 
-console.log("chips");
+inboxBtn.addEventListener("click", () => {
+  projectMain.textContent = ""
+  inboxMain()
+  createTask()
+})
 
-document.body.appendChild(component());
+todayBtn.addEventListener("click", () => {
+  projectMain.textContent = ""
+  todayMain()
+})
+
+weekBtn.addEventListener("click", () => {
+  projectMain.textContent = ""
+  weekMain()
+})
+// document.body.appendChild(component());
+
