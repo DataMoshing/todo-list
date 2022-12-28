dab66d5b5a61d61dd63a8b5ab340ae82b1e7c8a8
datamoshing <imkiloss@gmail.com>
2022-12-27 19:41:06 -0700

    Refactored code

--- src/index.js
+++ src/index.js
@@ -1,34 +1,5 @@
 // import _ from "lodash";
 import "./style.css";
-import createTask from "./task";
-import { inboxBtn, todayBtn, weekBtn, projectMain, inboxMain, todayMain, weekMain } from "./dom";
-
-// function component() {
-//   const element = document.createElement("div");
-
-//   // Lodash, currently included via a script, is required for this line to work
-//   element.innerHTML = _.join(["Hello", "webpack"], " ");
-//   element.classList.add("hello");
-
-//   return element;
-// }
-
-inboxMain()
-
-inboxBtn.addEventListener("click", () => {
-  projectMain.textContent = ""
-  inboxMain()
-  createTask()
-})
-
-todayBtn.addEventListener("click", () => {
-  projectMain.textContent = ""
-  todayMain()
-})
-
-weekBtn.addEventListener("click", () => {
-  projectMain.textContent = ""
-  weekMain()
-})
-// document.body.appendChild(component());
+import { } from "./dom";
+import { } from "./project"
 
