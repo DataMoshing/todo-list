9170b8bff4e2139df88c8ea5374019d3734237c4
datamoshing <imkiloss@gmail.com>
2022-12-30 08:55:51 -0700

    Renamed variables for readability

--- src/project.js
+++ src/project.js
@@ -1,21 +1,33 @@
+// import { projectMain } from "./dom";
 
+// import projectMain from "./dom";
+
+const projectHeader = document.querySelector(".project-main-header")
 const projectForm = document.querySelector("form")
-
 const todoList = [];
 
-const ProjectFactory = (newProject) => ({ newProject })
+const ProjectFactory = (project) => ({ project })
 
 const displayProject = () => {
     const projectContainer = document.querySelector(".project-container")
     projectContainer.textContent = ""
-    todoList.forEach((project, i) => {
+    todoList.forEach((newProject, i) => {
         const projectBtn = document.createElement("button");
-        projectBtn.textContent = project.newProject;
+        projectBtn.textContent = newProject.project;
         const projectDiv = document.createElement("div")
-        projectDiv.className = "project"
+        projectDiv.className = "project-div"
         projectDiv.setAttribute("data-project-index", i)
+
         projectDiv.append(projectBtn)
         projectContainer.append(projectDiv)
+
+        projectBtn.addEventListener("click", () => {
+            projectHeader.textContent = ""
+            // const addTaskBtn = document.createElement("button")
+            // addTaskBtn.textContent = "Add Task"
+            projectHeader.textContent = projectBtn.textContent
+            // projectHeader.append()
+        })
     })
 }
 
@@ -25,6 +37,7 @@ const addProject = (event) => {
     const createProject = ProjectFactory(newProject)
     todoList.push(createProject)
     displayProject()
+    console.log({ ...todoList })
     return createProject
 }
 
