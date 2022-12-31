df62afc35806429390a1e34bc2c81559e0201b3f
datamoshing <imkiloss@gmail.com>
2022-12-31 12:03:57 -0700

    Transferred DOM code to dom.js file

--- src/project.js
+++ src/project.js
@@ -1,45 +1,4 @@
-// import { projectMain } from "./dom";
-
-// import projectMain from "./dom";
-
-const projectHeader = document.querySelector(".project-main-header")
-const projectForm = document.querySelector("form")
-const todoList = [];
-
 const ProjectFactory = (project) => ({ project })
 
-const displayProject = () => {
-    const projectContainer = document.querySelector(".project-container")
-    projectContainer.textContent = ""
-    todoList.forEach((newProject, i) => {
-        const projectBtn = document.createElement("button");
-        projectBtn.textContent = newProject.project;
-        const projectDiv = document.createElement("div")
-        projectDiv.className = "project-div"
-        projectDiv.setAttribute("data-project-index", i)
 
-        projectDiv.append(projectBtn)
-        projectContainer.append(projectDiv)
-
-        projectBtn.addEventListener("click", () => {
-            projectHeader.textContent = ""
-            // const addTaskBtn = document.createElement("button")
-            // addTaskBtn.textContent = "Add Task"
-            projectHeader.textContent = projectBtn.textContent
-            // projectHeader.append()
-        })
-    })
-}
-
-const addProject = (event) => {
-    event.preventDefault()
-    const newProject = document.querySelector("#project-input").value
-    const createProject = ProjectFactory(newProject)
-    todoList.push(createProject)
-    displayProject()
-    console.log({ ...todoList })
-    return createProject
-}
-
-
-projectForm.addEventListener("submit", addProject)
+export default ProjectFactory
