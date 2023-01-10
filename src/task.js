c111efa158d3de72921b7e5f50fc7e29df7777ee
datamoshing <imkiloss@gmail.com>
2023-01-10 15:19:58 -0700

    Created task factory function

--- src/task.js
+++ src/task.js
@@ -1,20 +1,56 @@
 // import { format } from "date-fns";
 
+const todoTasks = []
+
+const createTask = (title, description, dueDate, priority) => {
+    const task = {
+        title,
+        description,
+        dueDate,
+        priority
+    }
+    todoTasks.push(task);
+}
 
 
-const TaskFactory = (task) =>
-    // const dueDate = () => {
-    //     const today = format(new Date(), "MM.dd.yyyy")
-    //     console.log(today)
-    // }
-    ({ task })
+createTask("Clean", "Wash dishes", "1/12/2023", "High")
+createTask("Groceries", "Buy grapes", "1/15/2023", "Low")
+
+console.log(todoTasks)
 
 
+export default createTask
 
-// const test = createTask("Title", "Description");
-// test.dueDate();
+// const personFactory = (name) => ({
+//     talk() {
+//         console.log(`Hello I am ${name}`)
+//     }
+// })
+
+// const me = personFactory("Bill")
+// me.talk()
 
 
-
-export default TaskFactory
+// const createTask = (title, description, dueDate, priority) => ({
+//     setTitle() {
+//         this.title = title
+//         console.log(`${title}`)
+//         return title
+//     },
+//     setDescription() {
+//         this.description = description
+//         return description
+//     },
+//     setDate() {
+//         this.dueDate = dueDate
+//         return dueDate
+//     },
+//     setPriority() {
+//         this.priority = priority
+//         return priority
+//     },
+//     setTask(task) {
+//         todoTasks.push(task)
+//     }
+// })
 
