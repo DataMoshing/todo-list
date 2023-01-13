852ad9ccb5f35faf576227be22da9eba09ff43e6
datamoshing <imkiloss@gmail.com>
2023-01-12 21:42:36 -0700

    Created project manager object and project factory function

--- src/project.js
+++ src/project.js
@@ -1,4 +1,32 @@
-const ProjectFactory = (project) => ({ project })
+// Project Manager object
+const PM = {
+    // Empty projects array
+    projects: [],
+    // Project method pushing to projects array
+    addProject(project) {
+        this.projects.push(project)
+    },
+}
+// Test logic
+// PM.addProject("Meep")
+// PM.addProject("Beep")
+
+const projectFactory = (project) => ({
+    project,
+    tasks: []
+})
+
+const project1 = projectFactory("Inbox");
+const project2 = projectFactory("Study");
+
+PM.addProject(project1)
+PM.addProject(project2);
+const createProject = (title) => {
+    const project = projectFactory(title)
+    PM.addProject(project)
+}
+
+console.log(PM.projects)
 
 
-export default ProjectFactory
+export { PM, createProject }
