ccb4136cd4d585d311b10a2559afa3ec9c32af31
datamoshing <imkiloss@gmail.com>
2023-01-14 11:42:47 -0700

    Created task factory

--- src/task.js
+++ src/task.js
@@ -1,56 +1,11 @@
-// import { format } from "date-fns";
+import PM from "./project"
 
-const todoTasks = []
+const taskFactory = (title, description, dueDate, priority) => ({ title, description, dueDate, priority })
 
 const createTask = (title, description, dueDate, priority) => {
-    const task = {
-        title,
-        description,
-        dueDate,
-        priority
-    }
-    todoTasks.push(task);
+    const task = taskFactory(title, description, dueDate, priority)
+    // PM.addTask(task)
+    return task
 }
 
-
-createTask("Clean", "Wash dishes", "1/12/2023", "High")
-createTask("Groceries", "Buy grapes", "1/15/2023", "Low")
-
-console.log(todoTasks)
-
-
-export default createTask
-
-// const personFactory = (name) => ({
-//     talk() {
-//         console.log(`Hello I am ${name}`)
-//     }
-// })
-
-// const me = personFactory("Bill")
-// me.talk()
-
-
-// const createTask = (title, description, dueDate, priority) => ({
-//     setTitle() {
-//         this.title = title
-//         console.log(`${title}`)
-//         return title
-//     },
-//     setDescription() {
-//         this.description = description
-//         return description
-//     },
-//     setDate() {
-//         this.dueDate = dueDate
-//         return dueDate
-//     },
-//     setPriority() {
-//         this.priority = priority
-//         return priority
-//     },
-//     setTask(task) {
-//         todoTasks.push(task)
-//     }
-// })
-
+export { taskFactory, createTask }
