6c8f8c0c94581535fea1a6cf959ff004086a3670
datamoshing <imkiloss@gmail.com>
2023-01-26 09:21:46 -0700

    Added uuid to tasks

--- src/task.js
+++ src/task.js
@@ -1,8 +1,11 @@
-const taskFactory = (title, description, dueDate, priority) => ({ title, description, dueDate, priority })
+// import { id } from "date-fns/locale";
+import { v4 as uuidv4 } from "uuid";
+
+const taskFactory = (title, description, dueDate, priority) => ({ title, description, dueDate, priority, id: uuidv4(), })
 
 const createTask = (title, description, dueDate, priority) => {
     const task = taskFactory(title, description, dueDate, priority)
     return task
 }
 
-export { taskFactory, createTask }
+export { createTask, taskFactory }
