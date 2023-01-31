d9bbf943edadf647ffc7b0642b433834f0b22d4f
datamoshing <imkiloss@gmail.com>
2023-01-30 22:36:12 -0700

    Can append projects to project list

--- src/dom.js
+++ src/dom.js
@@ -1,22 +1,33 @@
 import { projectFactory, PM } from "./project"
 
-// const projectList = document.querySelector(".project-list")
+const projectList = document.querySelector(".project-list")
 const projectForm = document.querySelector("form")
+const main = document.querySelector(".main")
 
+const displayProject = () => {
+    projectList.textContent = ""
+    PM.projects.forEach((project) => {
+        const projectDiv = document.createElement("div")
+        projectDiv.textContent = project.title
 
-// const displayProject = () => {
+        const projectContainer = document.createElement("div")
+        projectDiv.className = "project-div"
 
+        projectContainer.append(projectDiv)
+        projectList.append(projectDiv)
+        main.append(projectList)
 
-//     document.body.append(projectList)
-// }
-
+    })
+}
 
 const addProject = (event) => {
     event.preventDefault()
-    const projectInput = document.getElementsByName("project")[0].value
+    const projectInput = document.getElementById("project").value
+    console.log(projectInput)
     const createProject = projectFactory(projectInput)
-    console.log(createProject)
     PM.addProject(createProject)
+    displayProject()
     return createProject
 }
+
 projectForm.addEventListener("submit", addProject)
