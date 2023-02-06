99a446bee4bf3d94f161908086e5ee4695be2d6e
datamoshing <imkiloss@gmail.com>
2023-02-05 19:38:37 -0700

    Can delete projects from dom and array of objects

--- src/dom.js
+++ src/dom.js
@@ -7,54 +7,62 @@ const projectValue = projectForm.querySelector("input[name='add-project']")
 const taskList = document.querySelector(".task-list")
 const taskForm = document.querySelector(".task-form")
 const taskValue = taskForm.querySelector("input[name='add-task']")
+const taskDiv = document.createElement("div")
 
-const displayTask = () => {
-    // event.preventDefault()
-    const taskDiv = document.createElement("div")
-    const taskTitle = document.querySelector("#title").value
-    const taskDuedate = document.querySelector("#due-date").value
-    const taskPriority = document.querySelector("#task-priority").value
-    taskDiv.classList = "task-div"
-    taskDiv.append(taskTitle, taskDuedate, taskPriority)
-    taskList.append(taskDiv)
-}
+// const addTask = () => {
+//     const taskTitle = document.querySelector("#title").value
+//     const taskDuedate = document.querySelector("#due-date").value
+//     const taskPriority = document.querySelector("#task-priority").value
+//     const createTask = taskFactory(taskTitle, taskDuedate, taskPriority)
+//     console.log(createTask)
+// }
+
+// const displayTask = (event) => {
+//     event.preventDefault()
+//     const taskTitle = document.querySelector("#title").value
+//     const taskDuedate = document.querySelector("#due-date").value
+//     const taskPriority = document.querySelector("#task-priority").value
+//     taskDiv.classList = "task-div"
+//     taskDiv.append(taskTitle, taskDuedate, taskPriority)
+//     taskList.append(taskDiv)
+//     addTask()
+// }
 
 const displayProject = () => {
     // taskList.textContent = ""
     projectList.textContent = ""
     PM.projects.forEach((project) => {
-        console.log(project)
         const projectDiv = document.createElement("div")
+        const deleteBtn = document.createElement("button")
+        deleteBtn.textContent = "Delete"
+
         projectDiv.textContent = project.title
         projectDiv.setAttribute("project-index", project.id)
 
         const projectContainer = document.createElement("div")
         projectDiv.className = "project-div"
 
+        projectDiv.append(deleteBtn)
         projectContainer.append(projectDiv)
         projectList.append(projectContainer)
-        project.tasks.forEach(() => {
-            // const createTask = taskFactory(task.title, task.Duedate, task.priority)
-            // project.addTask(createTask)
-            // displayTask()
-            // console.log(task)
+
+        deleteBtn.addEventListener("click", (e) => {
+            e.preventDefault()
+            projectDiv.remove()
+            PM.deleteProject(project)
+            projectDiv.append(deleteBtn)
         })
     })
 }
 
-const addProject = (event) => {
-    event.preventDefault()
+const addProject = (e) => {
+    e.preventDefault()
     taskList.textContent = ""
-    const taskTitle = document.getElementById("title").value
-    const taskDuedate = document.getElementById("due-date").value
-    const taskPriority = document.getElementById("task-priority").value
     const projectInput = document.getElementById("project").value
-    const createTask = taskFactory(taskTitle, taskDuedate, taskPriority)
     const newProject = projectFactory(projectInput)
     PM.addProject(newProject)
-    newProject.addTask(createTask)
     displayProject()
 }
 
-taskValue.addEventListener("click", displayTask)
+// taskValue.addEventListener("click", displayTask)
 projectValue.addEventListener("click", addProject)
