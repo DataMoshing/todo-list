2d42990059130101a0802bfec27dc789b3e574da
datamoshing <imkiloss@gmail.com>
2023-02-11 19:54:50 -0700

    Can add/delete projects and add tasks to each project

--- src/dom.js
+++ src/dom.js
@@ -4,20 +4,23 @@ import taskFactory from "./task"
 const projectList = document.querySelector(".project-list")
 const projectForm = document.querySelector(".project-form")
 const projectValue = projectForm.querySelector("input[name='add-project']")
-const taskList = document.querySelector(".task-list")
+const taskList = document.createElement("div")
+const main = document.querySelector(".main-container")
+
+// const taskList = document.querySelector(".task-list")
 // const taskForm = document.querySelector(".task-form")
 // const taskValue = taskForm.querySelector("input[name='add-task']")
-const addTaskBtn = document.querySelector(".add-task-btn")
+// const addTaskBtn = document.querySelector(".add-task-btn")
 
 const createTask = () => {
     const taskTitle = document.getElementById("task-title").value
     const taskDuedate = document.getElementById("due-date").value
     const taskPriority = document.getElementById("task-priority").value
-    const taskDiv = document.createElement("div")
-    const taskContainer = document.createElement("div")
-    taskContainer.append(taskDiv)
-    taskList.append(taskContainer)
-    taskDiv.append(taskTitle, taskDuedate, taskPriority)
+    // const taskDiv = document.createElement("div")
+    // const taskContainer = document.createElement("div")
+    // taskContainer.append(taskDiv)
+    // taskList.append(taskContainer)
+    // taskDiv.append(taskTitle, taskDuedate, taskPriority)
     const newTask = taskFactory(taskTitle,
         taskDuedate, taskPriority)
     return newTask
@@ -39,7 +42,7 @@ const updateUI = (event) => {
         const deleteBtn = document.createElement("button")
         deleteBtn.textContent = "Delete"
 
-        projectDiv.textContent = project.title
+        projectDiv.textContent = `Project: ${project.title}`
         projectDiv.setAttribute("project-index", project.id)
 
         const projectContainer = document.createElement("div")
@@ -47,20 +50,43 @@ const updateUI = (event) => {
 
         projectDiv.append(deleteBtn)
         projectContainer.append(projectDiv)
+        projectContainer.classList = "project-container"
         projectList.append(projectContainer)
 
-        deleteBtn.addEventListener("click", () => {
-            projectDiv.remove()
-            PM.deleteProject(project)
-            projectDiv.append(deleteBtn)
-        })
+        const addTaskBtn = document.createElement("button")
+        addTaskBtn.textContent = "Add Task"
+        projectContainer.append(addTaskBtn)
+
         addTaskBtn.addEventListener("click", (e) => {
             e.preventDefault()
             const newTask = createTask()
             project.addTask(newTask)
             const taskDiv = document.createElement("div")
-            // taskDiv.textContent = newTask.title
+            taskDiv.classList = "task-div"
+            taskDiv.textContent = `Project: ${project.title} 
+            Task: ${newTask.title} Date: ${newTask.dueDate} Priority: ${newTask.priority}`
+            taskDiv.setAttribute("task-div", newTask.id)
+
+            const taskContainer = document.createElement("div")
+            taskContainer.classList = "task-container"
+            taskContainer.append(taskDiv)
             taskList.append(taskDiv)
+
+            main.append(taskList)
+        })
+        deleteBtn.addEventListener("click", (e) => {
+            e.preventDefault()
+            // projectDiv.remove()
+            addTaskBtn.remove()
+            PM.deleteProject(project)
+            const projectTasks = document.querySelectorAll(".task-div");
+            const projectContainers = document.querySelectorAll(".task-container")
+            for (let i = 0; i < projectTasks.length; i += 1) {
+                if (projectTasks[i].textContent.includes(project.title)) {
+                    projectContainers[i].remove()
+                    projectTasks[i].remove();
+                }
+            }
         })
     })
 }
