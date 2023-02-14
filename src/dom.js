4fb48addc7bf643dc53141e6261852d3681b2615
datamoshing <imkiloss@gmail.com>
2023-02-13 21:21:19 -0700

    Separated DOM task properties into own classes

--- src/dom.js
+++ src/dom.js
@@ -4,9 +4,10 @@ import taskFactory from "./task"
 const projectList = document.querySelector(".project-list")
 const projectForm = document.querySelector(".project-form")
 const projectValue = projectForm.querySelector("input[name='add-project']")
+// const inputValue = document.querySelector("task-title")
+const main = document.querySelector(".main-container")
 const taskList = document.createElement("div")
 taskList.classList = "task-list"
-const main = document.querySelector(".main-container")
 
 const createTask = () => {
     const taskTitle = document.getElementById("task-title").value
@@ -17,6 +18,7 @@ const createTask = () => {
     return newTask
 }
 
+
 const createProject = () => {
     const projectInput = document.getElementById("project").value
     const newProject = projectFactory(projectInput)
@@ -26,80 +28,103 @@ const createProject = () => {
 const updateUI = (e) => {
     e.preventDefault()
     const newProject = createProject()
-    PM.addProject(newProject)
-    projectList.textContent = ""
-    PM.projects.forEach((project) => {
-        const projectDiv = document.createElement("div")
-        const addTaskBtn = document.createElement("button")
-        addTaskBtn.className = "add-project-task"
-        const deleteProjectBtn = document.createElement("button")
-        deleteProjectBtn.textContent = "Delete"
-        deleteProjectBtn.className = "delete-project"
+    /* Iterate through each project in array and check if current project title is equal to new projects title if not create new project */
+    const projectExists = PM.projects.some((project) => project.title === newProject.title)
+    if (!projectExists) {
+        PM.addProject(newProject)
+        projectList.textContent = ""
+        PM.projects.forEach((project) => {
+            const projectDiv = document.createElement("h3")
+            const addTaskBtn = document.createElement("button")
+            addTaskBtn.className = "add-project-task"
+            const deleteProjectBtn = document.createElement("button")
+            deleteProjectBtn.textContent = "Delete"
+            deleteProjectBtn.className = "delete-project"
 
-        projectDiv.textContent = `Project: ${project.title}`
-        projectDiv.setAttribute("project-index", project.id)
+            projectDiv.textContent = `Project: ${project.title}`
+            projectDiv.setAttribute("project-index", project.id)
 
-        const projectContainer = document.createElement("div")
-        projectDiv.className = "project-div"
+            const projectContainer = document.createElement("div")
+            projectDiv.className = "project-div"
 
-        projectDiv.append(deleteProjectBtn)
-        projectContainer.append(projectDiv)
-        projectContainer.classList = "project-container"
-        projectList.append(projectContainer)
+            projectDiv.append(deleteProjectBtn)
+            projectContainer.append(projectDiv)
+            projectContainer.classList = "project-container"
+            projectList.append(projectContainer)
 
-        addTaskBtn.textContent = "Add Task"
-        projectContainer.append(addTaskBtn, deleteProjectBtn)
+            addTaskBtn.textContent = "Add Task"
+            projectContainer.append(addTaskBtn, deleteProjectBtn)
 
+            console.log(PM.projects)
 
-        addTaskBtn.addEventListener("click", (event) => {
-            event.preventDefault()
-            // Set variable to createTask function (taskFactory)
-            const newTask = createTask()
-            // Add new task to current project
-            project.addTask(newTask)
-            // Create div to display logic
-            const taskDiv = document.createElement("div")
-            taskDiv.classList = "task-div"
-            taskDiv.textContent = `Project: ${project.title} 
-            Task: ${newTask.title} Date: ${newTask.dueDate} Priority: ${newTask.priority}`
-            taskDiv.setAttribute("task-div", newTask.id)
+            addTaskBtn.addEventListener("click", (event) => {
+                event.preventDefault()
+                // Set variable to createTask function (taskFactory)
+                const newTask = createTask()
+                // Add new task to current project
+                project.addTask(newTask)
+                // Create elements to display logic
+                const taskDiv = document.createElement("div")
+                taskDiv.setAttribute("task-div", newTask.id)
+                const projectTask = document.createElement("project")
+                taskDiv.classList = "task-div"
+                const taskTitle = document.createElement("task")
+                const taskDate = document.createElement("due-date")
+                const taskPriority = document.createElement("priority")
 
-            const deleteTaskBtn = document.createElement("button")
-            deleteTaskBtn.textContent = "Delete Task"
-            taskDiv.append(deleteTaskBtn)
+                projectTask.textContent = `Project: ${project.title}`
+                projectTask.classList = "project-task"
+                taskTitle.classList = "task-title"
+                taskTitle.textContent =
+                    `Task: ${newTask.title}`
 
-            const taskContainer = document.createElement("div")
-            taskContainer.classList = "task-container"
-            // taskDiv.append(deleteTaskBtn)
-            taskContainer.append(taskDiv)
-            taskList.append(taskDiv)
+                taskDate.textContent = `Due-date: ${newTask.dueDate}`
+                taskDate.classList = "task-due-date"
 
-            main.append(taskList)
+                taskPriority.textContent = `Priority: ${newTask.priority}`
+                taskPriority.classList = "task-priority"
 
-            deleteTaskBtn.addEventListener("click", () => {
-                project.deleteTask(newTask)
-                taskDiv.remove()
+                const deleteTaskBtn = document.createElement("button")
+                deleteTaskBtn.textContent = "Delete Task"
+                deleteTaskBtn.classList = "delete-task-btn"
+
+                taskDiv.append(projectTask, taskTitle, taskDate, taskPriority, deleteTaskBtn)
+
+                const taskContainer = document.createElement("div")
+                taskContainer.classList = "task-container"
+                taskContainer.append(taskDiv)
+                taskList.append(taskDiv)
+
+                taskTitle.addEventListener("click", () => {
+                    const newInput = document.createElement("input")
+                    newInput.type = "text"
+                    taskTitle.append(newInput)
+                })
+
+                deleteTaskBtn.addEventListener("click", () => {
+                    project.deleteTask(newTask)
+                    taskDiv.remove()
+                })
+                main.append(taskList)
+            })
+            deleteProjectBtn.addEventListener("click", (event) => {
+                event.preventDefault()
+                PM.deleteProject(project)
+                projectDiv.remove()
+                addTaskBtn.remove()
+                deleteProjectBtn.remove()
+                // Select all task divs
+                const projectTasks = document.querySelectorAll(".task-div");
+                // Loop through all task divs in current project and remove
+                for (let i = 0; i < projectTasks.length; i += 1) {
+                    // If current project task includes project title remove project tasks
+                    if (projectTasks[i].textContent.includes(project.title)) {
+                        projectTasks[i].remove();
+                    }
+                }
             })
         })
-        deleteProjectBtn.addEventListener("click", (event) => {
-            event.preventDefault()
-            PM.deleteProject(project)
-            projectDiv.remove()
-            addTaskBtn.remove()
-            deleteProjectBtn.remove()
-            // Select all task divs
-            const projectTasks = document.querySelectorAll(".task-div");
-            // Loop through all task divs in current project and remove
-            for (let i = 0; i < projectTasks.length; i += 1) {
-                // If current project task includes project title remove project tasks
-                if (projectTasks[i].textContent.includes(project.title)) {
-                    projectTasks[i].remove();
-                }
-            }
-
-        })
-    })
+    }
 }
-
 projectValue.addEventListener("click", updateUI);
 // taskValue.addEventListener("click", createTask);
