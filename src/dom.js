ed1635fe7792a01db21c4a64d7f58d1bf5150e5c
DataMoshing <55223157+DataMoshing@users.noreply.github.com>
2023-09-09 15:07:47 -0700

    Reverted commit changes

--- src/dom.js
+++ src/dom.js
@@ -29,107 +29,175 @@ const createProject = () => {
     return newProject;
 };
 
-const renderProjects = () => {
-    // Clear project container
-    projectList.textContent = "";
-    // Loop through each project in PM projects array
-    PM.projects.forEach((project) => {
-        const projectContainer = document.createElement("div");
-        const projectDiv = document.createElement("h3");
-        const addTaskBtn = document.createElement("button");
-        const deleteProjectBtn = document.createElement("button");
-
-        projectDiv.textContent = `Project: ${project.title}`;
-        projectDiv.setAttribute("project-index", project.id);
-
-        projectDiv.className = "project-div";
-        projectContainer.className = "project-container";
-
-        addTaskBtn.className = "add-project-task";
-        addTaskBtn.textContent = "Add Task";
-        deleteProjectBtn.textContent = "Delete";
-        deleteProjectBtn.className = "delete-project";
-        projectDiv.append(deleteProjectBtn);
-        projectContainer.append(projectDiv);
-        projectList.append(projectContainer);
-        projectContainer.append(addTaskBtn, deleteProjectBtn);
-
-        addTaskBtn.addEventListener("click", (event) => {
-            event.preventDefault();
-            const newTask = createTask();
-            project.addTask(newTask);
-            const taskDiv = document.createElement("div");
-            const projectTask = document.createElement("project");
-            const taskTitle = document.createElement("task");
-            const taskDate = document.createElement("due-date");
-            const taskPriority = document.createElement("priority");
-            const deleteTaskBtn = document.createElement("button");
-            deleteTaskBtn.textContent = "✕";
-
-            projectTask.textContent = `Project: ${project.title}`;
-            taskTitle.textContent = `Task: ${newTask.title}`;
-            taskDate.textContent = `Due-date: ${newTask.dueDate}`;
-            taskPriority.textContent = `Priority: ${newTask.priority}`;
-
-            projectTask.className = "project-task";
-            taskDiv.className = "task-div";
-            taskDiv.setAttribute("task-div", newTask.id);
-            taskTitle.className = "task-title";
-            taskDate.className = "task-due-date";
-            taskPriority.className = "task-priority";
-            deleteTaskBtn.className = "delete-task-btn";
-
-            taskDiv.append(deleteTaskBtn, projectTask, taskTitle, taskDate, taskPriority);
-
-            const taskContainer = document.createElement("div");
-            taskContainer.classList = "task-container";
-            taskContainer.append(taskDiv);
-
-            taskList.append(taskDiv);
-
-            deleteTaskBtn.addEventListener("click", () => {
-                project.deleteTask(newTask);
-                taskDiv.remove();
-            });
-
-            main.append(taskList);
-        });
-
-        deleteProjectBtn.addEventListener("click", (event) => {
-            event.preventDefault();
-            PM.deleteProject(project);
-            projectDiv.remove();
-            addTaskBtn.remove();
-            deleteProjectBtn.remove();
-            const projectTasks = document.querySelectorAll(".task-div");
-            // Loop through tasks and remove tasks associated with project from DOM.
-            projectTasks.forEach((taskDiv) => {
-                if (taskDiv.textContent.includes(project.title)) {
-                    taskDiv.remove();
-                }
-            });
-        });
-    });
-};
-
-document.addEventListener("DOMContentLoaded", renderProjects);
-
-// Update UI with new projects.
 const updateUI = (e) => {
-    e.preventDefault();
-    const newProject = createProject();
-    // If project title is empty return.
-    if (!newProject) {
-        return;
+    e.preventDefault()
+    const newProject = createProject()
+    if (newProject.title === "") {
+        return alert("Project name can't be empty")
     }
-    // Check if project with the same title already exists.
-    const projectExists = PM.projects.some((project) => project.title === newProject.title);
-    // If project doesn't exist add to PM project array and render project.
+    /* Iterate through each project in array and check if current project title is equal to new projects title if not create new project */
+    const projectExists = PM.projects.some((project) => project.title === newProject.title)
     if (!projectExists) {
-        PM.addProject(newProject);
-        renderProjects();
+        PM.addProject(newProject)
+        projectList.textContent = ""
+        PM.projects.forEach((project) => {
+            // Create projects
+            const projectContainer = document.createElement("div")
+            const projectDiv = document.createElement("h3")
+            const addTaskBtn = document.createElement("button")
+            const deleteProjectBtn = document.createElement("button")
+
+            projectDiv.textContent = `Project: ${project.title}`
+            projectDiv.setAttribute("project-index", project.id)
+
+            projectDiv.className = "project-div"
+            projectContainer.className = "project-container"
+
+            addTaskBtn.className = "add-project-task"
+            addTaskBtn.textContent = "Add Task"
+            deleteProjectBtn.textContent = "Delete"
+            deleteProjectBtn.className = "delete-project"
+            projectDiv.append(deleteProjectBtn)
+            projectContainer.append(projectDiv)
+            projectList.append(projectContainer)
+            projectContainer.append(addTaskBtn, deleteProjectBtn)
+
+
+            addTaskBtn.addEventListener("click", (event) => {
+                event.preventDefault()
+                // Set variable to createTask function (taskFactory)
+                const newTask = createTask()
+                console.log(newTask)
+                // Add new task to current project
+                console.log(project)
+                project.addTask(newTask)
+                // Create elements to display logic
+                const taskDiv = document.createElement("div")
+                const projectTask = document.createElement("project")
+                const taskTitle = document.createElement("task")
+                const taskDate = document.createElement("due-date")
+                const taskPriority = document.createElement("priority")
+                const deleteTaskBtn = document.createElement("button")
+                deleteTaskBtn.textContent = "✕"
+
+                projectTask.textContent = `Project: ${project.title}`
+                taskTitle.textContent = `Task: ${newTask.title}`
+                taskDate.textContent = `Due-date: ${newTask.dueDate}`
+                taskPriority.textContent = `Priority: ${newTask.priority}`
+
+                projectTask.className = "project-task"
+                taskDiv.className = "task-div"
+                taskDiv.setAttribute("task-div", newTask.id)
+                taskTitle.className = "task-title"
+                taskDate.className = "task-due-date"
+                taskPriority.className = "task-priority"
+                deleteTaskBtn.className = "delete-task-btn"
+
+                taskDiv.append(deleteTaskBtn, projectTask, taskTitle, taskDate, taskPriority)
+
+                const taskContainer = document.createElement("div")
+                taskContainer.classList = "task-container"
+                taskContainer.append(taskDiv)
+
+                taskList.append(taskDiv)
+
+                // Edit task elements
+                const newForm = document.createElement("form")
+                const newInput = document.createElement("input")
+                const newTaskLabel = document.createElement("label")
+                const editTaskBtn = document.createElement("button")
+                const editPriorityBtn = document.createElement("button")
+                const editDueDate = document.createElement("button")
+                const newPriority = document.createElement("select")
+                const newDueDate = document.createElement("input")
+
+                editTaskBtn.textContent = "Edit task"
+                editTaskBtn.className = "edit-task-btn"
+                editPriorityBtn.textContent = "Edit Priority"
+                editDueDate.textContent = "Edit Due date"
+                editDueDate.className = "edit-due-date-btn"
+                editPriorityBtn.className = "edit-priority-btn"
+                newInput.id = "new-task-input"
+
+                // Edit current task 
+                taskTitle.addEventListener("click", () => {
+                    newInput.type = "text"
+                    newInput.name = "new-task-input"
+                    newTaskLabel.setAttribute("for", "updated-task")
+                    newTaskLabel.textContent = "Update task: "
+                    newTaskLabel.append(newInput)
+                    newForm.append(newTaskLabel)
+                    taskDiv.append(newForm, editTaskBtn)
+                })
+
+                editTaskBtn.addEventListener("click", () => {
+                    project.updateTask(newTask.id, { title: `${newInput.value}` })
+                    taskTitle.textContent = `Task: ${newInput.value}`
+                    newTaskLabel.remove()
+                    newInput.remove()
+                    editTaskBtn.remove()
+                })
+
+                taskPriority.addEventListener("click", () => {
+                    const newPriorityLow = document.createElement("option");
+                    const newPriorityMedium = document.createElement("option");
+                    const newPriorityHigh = document.createElement("option");
+                    newPriorityLow.text = "Low"
+                    newPriorityMedium.text = "Medium"
+                    newPriorityHigh.text = "High"
+                    newPriority.textContent = "New Priority: "
+                    newPriority.className = "new-priority"
+                    newPriority.append(newPriorityLow, newPriorityMedium, newPriorityHigh)
+                    taskDiv.append(newPriority, editPriorityBtn)
+                })
+
+                editPriorityBtn.addEventListener("click", () => {
+                    project.updateTask(newTask.id, { priority: `${newPriority.value}` })
+                    taskPriority.textContent = `Priority: ${newPriority.value}`
+                    newPriority.remove()
+                    editPriorityBtn.remove()
+                })
+
+                taskDate.addEventListener("click", () => {
+                    newDueDate.setAttribute("type", "date")
+                    newDueDate.className = "new-due-date"
+                    taskDiv.append(newDueDate, editDueDate)
+                })
+
+                editDueDate.addEventListener("click", () => {
+                    project.updateTask(newTask.id, { dueDate: `${newDueDate.value}` })
+                    taskDate.textContent = `Due-date ${newDueDate.value}`
+                    newDueDate.remove()
+                    editDueDate.remove()
+                })
+                // Remove single task
+                deleteTaskBtn.addEventListener("click", () => {
+                    project.deleteTask(newTask)
+                    taskDiv.remove()
+                })
+                main.append(taskList)
+            })
+            // Delete project + tasks
+            deleteProjectBtn.addEventListener("click", (event) => {
+                event.preventDefault()
+                PM.deleteProject(project)
+                projectDiv.remove()
+                addTaskBtn.remove()
+                deleteProjectBtn.remove()
+                // Select all task divs
+                const projectTasks = document.querySelectorAll(".task-div");
+                // Loop through all task divs in current project and remove
+                for (let i = 0; i < projectTasks.length; i += 1) {
+                    // If current project task includes project title remove project tasks
+                    if (projectTasks[i].textContent.includes(project.title)) {
+                        projectTasks[i].remove();
+                    }
+                }
+            })
+        })
     }
-};
+    return false
+}
 
 projectValue.addEventListener("click", updateUI);
 
--- src/project.js
+++ src/project.js
@@ -2,73 +2,51 @@ import { v4 as uuidv4 } from "uuid";
 
 const PM = {
     projects: [],
-    // Get projects from local storage, if project found parse project and assign to projects property.
-    localStorage() {
-        const storedProjects = localStorage.getItem("projects");
-        if (storedProjects) {
-            this.projects = JSON.parse(storedProjects);
-        }
-    },
     addProject(project) {
-        this.projects.push(project);
-        // Update local storage
-        localStorage.setItem("projects", JSON.stringify(this.projects));
+        this.projects.push(project)
     },
-
     deleteProject(project) {
-        const projectIndex = this.projects.indexOf(project);
+        const projectIndex = this.projects.indexOf(project)
         if (projectIndex !== -1) {
-            this.projects.splice(projectIndex, 1);
-            localStorage.setItem("projects", JSON.stringify(this.projects));
-            return true;
+            this.projects.splice(projectIndex, 1)
+            return true
         }
-        return false;
+        return false
     },
     updateProject(projectId, newTitle) {
         // Iterate through projects array
         for (let i = 0; i < this.projects.length; i += 1) {
             // Check current iteration project id to project id
             if (this.projects[i].id === projectId) {
-                // If title hasn't changed return false
-                if (this.projects[i].title === newTitle) return false;
-                // Else update project title with new title
-                this.projects[i].title = newTitle;
-                this.updateLocalStorage();
-                return true;
+                // If title hasnt changed return false
+                if (this.projects[i].title === newTitle) return false
+                // Else return project title with new title
+                this.projects[i].title = newTitle
+                return true
             }
         }
-        return false;
+        return false
     },
-    // Update local storage by stringifying current projects array and store under key "projects".
-    updateLocalStorage() {
-        localStorage.setItem("projects", JSON.stringify(this.projects));
-    }
-};
+}
 
 const projectFactory = (title) => ({
     title,
     tasks: [],
     id: uuidv4(),
     addTask(task) {
-        this.tasks.push(task);
-        PM.updateLocalStorage();
+        this.tasks.push(task)
     },
     deleteTask(task) {
-        const taskIndex = this.tasks.indexOf(task);
+        const taskIndex = this.tasks.indexOf(task)
         if (taskIndex !== -1) {
-            this.tasks.splice(taskIndex, 1);
-            PM.updateLocalStorage();
-            return true;
+            this.tasks.splice(taskIndex, 1)
+            return true
         }
-        return false;
+        return false
     },
     updateTask(taskId, data) {
-        Object.assign(this.tasks.find((el) => el.id === taskId), data);
-        PM.updateLocalStorage();
+        Object.assign(this.tasks.find(el => el.id === taskId), data)
     }
-});
-
-// Call the localStorage function to retrieve projects from local storag
-PM.localStorage();
+})
 
 export { projectFactory, PM }
